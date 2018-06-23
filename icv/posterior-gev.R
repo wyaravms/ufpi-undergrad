6a007331b6ea994ef91c372a93d3f051d44b0ca4
Wyara Moura <wyaravms@hotmail.com>
2018-06-23 13:27:13 -0700

    Update posterior-gev.R

--- icv/posterior-gev.R
+++ icv/posterior-gev.R
@@ -13,10 +13,8 @@ posterior.gev=function (data, block, int = 1000) {
     n = length(data)
     lpost = function(mu, sigma, xi) {
       logpost = -n * log(sigma) - sum((1 + xi * (data - mu)/sigma)^(-1/xi))
-      logpost = logpost - (1 + 1/xi) * sum(log((1 + xi * (data - 
-                                                            mu)/sigma)))
-      logpost = logpost + (0.001 - 1) * log(sigma) - 0.001 * 
-        sigma - mu^2/2000 - xi^2/200
+      logpost = logpost - (1 + 1/xi) * sum(log((1 + xi * (data - mu)/sigma)))
+      logpost = logpost + (0.001 - 1) * log(sigma) - 0.001 * sigma - mu^2/2000 - xi^2/200
       logpost
     }
     mumc = array(0, c(burnin + int, 1))
@@ -36,20 +34,15 @@ posterior.gev=function (data, block, int = 1000) {
     for (i in 2:burnin) {
       muest = rnorm(1, mumc[i - 1], Vu)
       xiest = rnorm(1, ximc[i - 1], Vxi)
-      sigmaest = rgamma(1, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 
-                                                              1]/Vsigma)
+      sigmaest = rgamma(1, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 1]/Vsigma)
       while (min(1 + xiest * (data - muest)/sigmaest) < 0) {
         muest = rnorm(1, mumc[i - 1], Vu)
         xiest = rnorm(1, ximc[i - 1], Vxi)
-        sigmaest = rgamma(1, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 
-                                                                1]/Vsigma)
+        sigmaest = rgamma(1, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 1]/Vsigma)
       }
-      alpha = exp(lpost(muest, sigmaest, xiest) - lpost(mumc[i - 
-                                                               1], sigmamc[i - 1], ximc[i - 1]))
-      alpha = alpha * dgamma(sigmamc[i - 1], sigmaest^2/Vsigma, 
-                             sigmaest/Vsigma)
-      alpha = alpha/(dgamma(sigmaest, sigmamc[i - 1]^2/Vsigma, 
-                            sigmamc[i - 1]/Vsigma))
+      alpha = exp(lpost(muest, sigmaest, xiest) - lpost(mumc[i - 1], sigmamc[i - 1], ximc[i - 1]))
+      alpha = alpha * dgamma(sigmamc[i - 1], sigmaest^2/Vsigma, sigmaest/Vsigma)
+      alpha = alpha/(dgamma(sigmaest, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 1]/Vsigma))
       if (is.nan(alpha)) {
         alpha = 0
       }
@@ -72,20 +65,15 @@ posterior.gev=function (data, block, int = 1000) {
     for (i in (burnin + 1):(burnin + thin * int)) {
       muest = rnorm(1, mumc[i - 1], Vu)
       xiest = rnorm(1, ximc[i - 1], Vxi)
-      sigmaest = rgamma(1, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 
-                                                              1]/Vsigma)
+      sigmaest = rgamma(1, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 1]/Vsigma)
       while (min(1 + xiest * (data - muest)/sigmaest) < 0) {
         muest = rnorm(1, mumc[i - 1], Vu)
         xiest = rnorm(1, ximc[i - 1], Vxi)
-        sigmaest = rgamma(1, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 
-                                                                1]/Vsigma)
+        sigmaest = rgamma(1, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 1]/Vsigma)
       }
-      alpha = exp(lpost(muest, sigmaest, xiest) - lpost(mumc[i - 
-                                                               1], sigmamc[i - 1], ximc[i - 1]))
-      alpha = alpha * dgamma(sigmamc[i - 1], sigmaest^2/Vsigma, 
-                             sigmaest/Vsigma)
-      alpha = alpha/(dgamma(sigmaest, sigmamc[i - 1]^2/Vsigma, 
-                            sigmamc[i - 1]/Vsigma))
+      alpha = exp(lpost(muest, sigmaest, xiest) - lpost(mumc[i - 1], sigmamc[i - 1], ximc[i - 1]))
+      alpha = alpha * dgamma(sigmamc[i - 1], sigmaest^2/Vsigma, sigmaest/Vsigma)
+      alpha = alpha/(dgamma(sigmaest, sigmamc[i - 1]^2/Vsigma, sigmamc[i - 1]/Vsigma))
       if (is.nan(alpha)) {
         alpha = 0
       }
